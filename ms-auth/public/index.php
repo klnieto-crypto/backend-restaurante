820b80da6c853afd620fee42ffe6a1852bfc7702
LuzIA0612 <klucia1206@gmail.com>
2026-06-07 15:26:50 -0500

    karen: [Punto de entrada index.php en los 4 microservicios]

---
+++ ms-auth/public/index.php
@@ -0,0 +1,20 @@
+<?php
+// karen: [Punto de entrada ms-auth]
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+require __DIR__ . '/../app/Config/Database.php';
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, true, true);
+$app->addBodyParsingMiddleware();
+
+require __DIR__ . '/../app/Routes/routes.php';
+
+$app->run();
