02de3da5018f92e71af3d0817be0143651fa1576
LuzIA0612 <klucia1206@gmail.com>
2026-06-10 15:30:09 -0500

    karen: [Configuración CORS en los 4 microservicios]

--- ms-productos/public/index.php
+++ ms-productos/public/index.php
@@ -1,5 +1,5 @@
 <?php
-// karen: [Punto de entrada ms-productos]
+// karen: [Punto de entrada ms-auth con CORS]
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
@@ -12,8 +12,23 @@ require __DIR__ . '/../app/Config/Database.php';
 
 $app = AppFactory::create();
 
+// CORS
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+$app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 $app->addBodyParsingMiddleware();
+
+// OPTIONS preflight
+$app->options('/{routes:.+}', function ($request, $response) {
+    return $response;
+});
 
 require __DIR__ . '/../app/Routes/routes.php';
 
